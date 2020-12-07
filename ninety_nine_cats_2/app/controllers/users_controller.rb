b8f56704ee997e643b1ffd7570843b22c8bf5e62
jane baik <janebaik@berkeley.edu>
2020-12-07 14:27:31 -0800

    finishing up user_controller create

--- ninety_nine_cats_2/app/controllers/users_controller.rb
+++ ninety_nine_cats_2/app/controllers/users_controller.rb
@@ -4,7 +4,14 @@ class UsersController < ApplicationController
     end
 
     def create 
-        @user = User.new(user_params).
-
+        @user = User.new(user_params)
+        if user.save
+            session[:session_token] = @user.session_token
     end
+
+    def user_params
+        params.require(:user).permit(:user_name, :password)
+    end
+
+
 end
