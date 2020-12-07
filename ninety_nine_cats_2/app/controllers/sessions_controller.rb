5cb388a0c30642155dc8b9740170b535a59e2be5
soura934 <soura934@gmail.com>
2020-12-07 17:17:57 -0500

    doing users_controller

--- ninety_nine_cats_2/app/controllers/sessions_controller.rb
+++ ninety_nine_cats_2/app/controllers/sessions_controller.rb
@@ -5,10 +5,18 @@ class SessionsController < ApllicationsController
     end
 
     def create 
+        debugger
         # Verify the user_name/password
         # Reset the User's session_token
         # Update the session hash with the new session_token
         # Redirect the user to the cats index page
+        user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+        if user
+            new_session_token = user.reset_session_token!
+            session[:session_token] = new_session_token
+            redirect_to cats_url
+            # put an else when user's info is wrong
+        end
     end
     
 
